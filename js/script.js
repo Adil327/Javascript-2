653ca320b028151e649a2802e5cba67e1b25c824
Adil327 <adilsajjad192@gmail.com>
2024-03-24 11:19:22 +0500

    First Commit: Created html, css and js files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// clear Statement
+document.getElementById("clearInputBtn").onclick = function () {
+    document.getElementById("inputDiv").innerHTML = " ";
+}
+
+// clear output
+document.getElementById("clearOutputBtn").onclick = function () {
+    document.getElementById("outputDiv").innerHTML = " ";
+}
+
+document.getElementById("concateBtn").onclick = function () {
+    document.getElementById("")
+}
